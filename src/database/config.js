33c9462d7771f463c4d5837f7edb156793c771eb
DeveloperFromIndia <gnchrv.dll@gmail.com>
2024-07-22 21:08:24 +0300

    removing fuckup

--- src/database/config.js
+++ src/database/config.js
@@ -2,13 +2,9 @@ import Sequelize from "sequelize";
 import dotenv from 'dotenv'; dotenv.config();
 
 const config = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
     {
-        dialect: 'postgres',
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
+        dialect: 'sqlite',
+        storage: 'src/database/cartel.sqlite',
         logging: false, // dev
         define: {
             freezeTableName: true,
